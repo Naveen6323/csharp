36583acb8320e2b224ed2a823e57232f537bcc33
Naveen6323 <naveenveerabattini@gmail.com>
2025-01-23 11:41:02 +0530

    .

--- practice problems/Program.cs
+++ practice problems/Program.cs
@@ -11,8 +11,7 @@ namespace practice_problems
 
         public int add(int n, int m)
         {
-            Console.WriteLine("change");
-            return n;
+            return n+m;
 
         }
 
